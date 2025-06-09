72c9f609d6ef096b34b671c02b17898cf1a5a172
crybot <crybot@hotmail.it>
2025-06-09 11:58:59 +0200

    Add new target binary for creating binary files

---
+++ src/create_file.cpp
@@ -0,0 +1,15 @@
+#include <print>
+#include "utils.hpp"
+
+auto main(int argc, char* argv[]) -> int {
+  const int num_records = std::stoi(argv[1]);
+  const uint32_t max_payload_length = std::stoi(argv[2]);
+  constexpr uint32_t seed = 42;
+  auto path = std::filesystem::path(argv[3]);
+  files::generateRandomFile(path, num_records, max_payload_length, seed);
+
+  std::print(
+      "File created succesfully at {}/{}\n", std::filesystem::current_path().string(), path.string()
+  );
+  return 0;
+}
