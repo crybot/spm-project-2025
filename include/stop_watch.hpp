b43bdfa42865b2fe0d3a0b773a28c65004dae196
crybot <crybot@hotmail.it>
2025-06-19 12:56:00 +0200

    Compute running average elapsed times in StopWatch

--- include/stop_watch.hpp
+++ include/stop_watch.hpp
@@ -5,10 +5,15 @@
 template <typename Duration = std::chrono::milliseconds>
 class StopWatch {
  public:
-  StopWatch(std::string_view description = "Benchmark results", bool print_last = true)
+  StopWatch(
+      std::string_view description = "Benchmark results",
+      bool print_last = true,
+      bool compute_statistics = false
+  )
       : description_{std::move(description)},
         begin_time_{std::chrono::high_resolution_clock::now()},
-        print_last_{print_last} {
+        print_last_{print_last},
+        compute_statistics_{compute_statistics} {
   }
   ~StopWatch() {
     if (print_last_) {
@@ -25,12 +30,21 @@ class StopWatch {
   std::string description_;
   std::chrono::high_resolution_clock::time_point begin_time_;
   bool print_last_;
+  bool compute_statistics_;
+  std::size_t count_{0};
+  double mean_{0.0};
 
   auto printElapsed() -> void {
     const auto end_time = std::chrono::high_resolution_clock::now();
     const auto elapsed = std::chrono::duration_cast<Duration>(end_time - begin_time_);
 
     std::print("{}\nElapsed time ({}): {}\n\n", description_, getDurationSuffix(), elapsed.count());
+
+    if (compute_statistics_) {
+      count_ += 1;
+      mean_ = (static_cast<double>(count_ - 1) * mean_ + elapsed.count()) / count_;
+      std::print("{}\nRunning mean - elapsed time ({}): {}\n\n", description_, getDurationSuffix(), mean_);
+    }
   }
 
   static consteval auto getDurationSuffix() -> const char* {
